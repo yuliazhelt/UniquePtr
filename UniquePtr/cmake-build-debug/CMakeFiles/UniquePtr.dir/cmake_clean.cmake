b60cb5d872e73ecc328e2d418bca7ff848abbd82
yuliazhelt <77640129+yuliazhelt@users.noreply.github.com>
2021-10-21 19:19:51 +0300

    Add files via upload

---
+++ UniquePtr/cmake-build-debug/CMakeFiles/UniquePtr.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/UniquePtr.dir/main.cpp.o"
+  "UniquePtr"
+  "UniquePtr.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/UniquePtr.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ UniquePtr/cmake-build-debug/CMakeFiles/UniquePtr.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for UniquePtr.
+# This may be replaced when dependencies are built.
